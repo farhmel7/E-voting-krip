d8e762802c299195bbdd1060a252dfc7dd96c8e5
0farhmel7 <akhmadfarhan929@gmail.com>
2026-07-11 20:54:55 +0900

    Perbaiki pembacaan env di PythonAnywhere

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ import shutil
 from dotenv import load_dotenv
 
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(BASE_DIR)
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 from evoting_system import SecureEVotingSystem
 
